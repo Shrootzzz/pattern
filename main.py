eef96a45b1c98fd00c877db140984f76cf91e927
Kevin Mao <km3290@columbia.edu>
2018-02-18 04:22:49 -0500

    48 articles

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ nyp = newspaper.build('https://nypost.com/news/', memoize_articles=False)
 google = newspaper.build('https://news.google.com', memoize_articles=False)
 archive = newspaper.build('http://www.gunviolencearchive.org', memoize_articles=False)
 foxnews = newspaper.build('http://www.foxnews.com', memoize_articles=False)
+ex1 = Article('https://www.cnn.com/2017/12/14/us/sandy-hook-newtown-shooting-victims-profiles/index.html')
+ex2 = Article('https://www.cbsnews.com/news/pulse-nightclub-shooting-orlando/')
+ex3 = Article('https://www.nbcnews.com/news/us-news/beginning-era-1966-university-texas-clock-tower-shooting-n620556')
+ex4 = Article('https://www.cnn.com/2016/09/28/us/south-carolina-elementary-school-shooting/index.html')
+ex5 = Article('http://www.latimes.com/local/lanow/la-me-ln-norcal-elementary-school-shooting-20171114-story.html')
+ex1.download()
+ex2.download()
+ex3.download()
+ex4.download()
+ex5.download()
 
 papers = [washington, nyp, google, archive, foxnews]
 news_pool.set(papers, threads_per_source=2)
@@ -17,6 +27,11 @@ num = 0
 data = []
 articles = []
 
+articles.append(ex1)
+articles.append(ex2)
+articles.append(ex3)
+articles.append(ex4)
+articles.append(ex5)
 articles.extend(washington.articles)
 articles.extend(nyp.articles)
 articles.extend(google.articles)
