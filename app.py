f1ed06f759921b750a0b797d2e16d164fb13e293
William Wang <William@dyn-160-39-139-162.dyn.columbia.edu>
2018-02-18 00:59:10 -0500

    stupid

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-import wikipedia
 import re
 
-pages = wikipedia.search("shooting")
-print(pages)
-i=0
+templates = []
+segments = []
+filename = "article_url.txt"
 
-pattern = re.compile("\s+[a-z|A-Z|\s]+\s+shot\s+[a-z|A-Z|\s]+\s+")
-# group = pattern.match(") is an American politician who ")
-# print(group)
+pattern = re.compile("[a-z|A-Z|0-9|\,|\-|]*\swoman\s+[a-z|A-Z|\s|0-9|\,|\-|]*\s*was killed\s[a-z|A-Z|0-9|\,|\-|]*")
+#\s+[a-z|A-Z|\s|0-9|\,|\-]+\s+
 
-for page in pages:
-	if "disambiguation" in page:
-		continue
-	myGroup = pattern.search(wikipedia.page(page).content)
-	print(page)
-	print(myGroup)
+myFile = open(filename,"r")
+
+text = myFile.read()
+sentences = text.split(".")
+
+for sentence in sentences:
+	result = re.search(pattern,str(sentence))
+	if result:
+		myPattern = result.group(0)
 		
